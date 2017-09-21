f2c76c24294c8b2154598381f6feefe1d6df5938
NguyễnHan2901 <hanntd00426@fpt.edu.vn>
2017-09-21 11:27:05 +0700

    HomeWork

--- src/student/Student.java
+++ src/student/Student.java
@@ -20,55 +20,58 @@ import javax.swing.JTextField;
 public class Student {
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Add Student");
-        frame.setSize(1000, 700);
+        JFrame frame = new JFrame("Student");
+        frame.setSize(900, 600);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
         frame.setLayout(null);
         JPanel panel = new JPanel(null);
-        panel.setBounds(100, 50, 900, 700);
+        panel.setBounds(100, 30, 700, 500);
 
         panel.setBorder(BorderFactory.createEmptyBorder(1, 1, 1, 1));
         panel.setBackground(Color.GRAY);
 
-        JLabel lblId = new JLabel("ID: ");
-        lblId.setBounds(50, 100, 50, 50);
+        JLabel lblId = new JLabel("ID ");
+        lblId.setBounds(300, 100, 100, 40);
         
         JTextField txtId = new JTextField();
-        txtId.setBounds(105, 100, 100, 50);
+        txtId.setBounds(400, 100, 205, 40);
         
-        JLabel lblName = new JLabel("Name: ");
-        lblName.setBounds(50, 160, 50, 50);
+        JLabel lblName = new JLabel("Name ");
+        lblName.setBounds(300, 160, 100, 40);
         
         JTextField txtName = new JTextField();
-        txtName.setBounds(105, 160, 100, 50);
+        txtName.setBounds(400, 160, 205, 40);
         
-        JLabel lblEmail = new JLabel("Emai: ");
-        lblEmail.setBounds(50, 220, 50, 50);
+        JLabel lblEmail = new JLabel("Emai ");
+        lblEmail.setBounds(300, 220, 100, 40);
         
         JTextField txtEmail = new JTextField();
-        txtEmail.setBounds(105, 220, 100, 50);
+        txtEmail.setBounds(400, 220, 205, 40);
         
-        JLabel lblPhone = new JLabel("Phone: ");
-        lblPhone.setBounds(50, 280, 50, 50);
+        JLabel lblPhone = new JLabel("Phone ");
+        lblPhone.setBounds(300, 280, 100, 40);
         
         JTextField txtPhone = new JTextField();
-        txtPhone.setBounds(105, 280, 100, 50);
+        txtPhone.setBounds(400, 280, 205, 40);
         
-        JLabel lblRollNum = new JLabel("Roll Number: ");
-        lblRollNum.setBounds(50, 340, 50, 50);
+        JLabel lblRollNum = new JLabel("Roll Number ");
+        lblRollNum.setBounds(300, 340, 100, 40);
         
         JTextField txtRoll = new JTextField();
-        txtRoll.setBounds(105, 340, 100, 50);
+        txtRoll.setBounds(400, 340, 205, 40);
         
-        JLabel lblClass = new JLabel("Class Name: ");
-        lblClass.setBounds(50, 400, 50, 50);
+        JLabel lblClass = new JLabel("Class Name ");
+        lblClass.setBounds(300, 400, 100, 40);
         
         JTextField txtClass = new JTextField();
-        txtClass.setBounds(105, 400, 100, 50);
+        txtClass.setBounds(400, 400, 205, 40);
         
         JButton btnLogin = new JButton("Login");
-        btnLogin.setBounds(205, 420, 100, 40);
+        btnLogin.setBounds(400, 450, 100, 40);
+        
+        JButton btnCancel = new JButton("Cancel");
+        btnCancel.setBounds(505, 450, 100, 40);
         
 
         panel.add(lblId);
@@ -89,8 +92,10 @@ public class Student {
         panel.add(lblClass);
         panel.add(txtClass);
         panel.add(btnLogin);
+        panel.add(btnCancel);
         frame.add(panel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
+    
 
 }
